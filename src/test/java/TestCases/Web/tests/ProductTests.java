40a5ab1eacb5f4dce1692b6ef17cf2e582ee1899
shohel677 <shohel877@gmail.com>
2024-04-30 23:40:19 +0600

    Testcase update

--- src/test/java/TestCases/Web/tests/ProductTests.java
+++ src/test/java/TestCases/Web/tests/ProductTests.java
@@ -4,31 +4,24 @@ import TestComponents.Web.WebBaseTest;
 import org.testng.annotations.Test;
 public class ProductTests extends WebBaseTest {
 
+    @Test(description = "Verify if home page is opened")
+    public void testToVerifyHomepage() {
+        testSteps.openingProduct();
+
+    }
+
     @Test(description = "Test to verify add to cart")
-    public void testToVerifyAddToCart() {
+    public void testToVerifyAddToCart()  {
+        testSteps.openingProduct();
+        testSteps.clickingAddToCart2();
+    }
+    @Test(description = "Test to verify product count")
+    public void testToVerifyProductCount() {
         testSteps.openingProduct();
         testSteps.clickingAddToCart();
         testSteps.checkingProductCount();
     }
 
-    @Test(description = "Test to verify add to cart2")
-    public void testToVerifyAddToCart2()  {
-        testSteps.openingProduct();
-        testSteps.clickingAddToCart2();
-        testSteps.checkingProductCount();
-    }
-    @Test(description = "Test to verify add to cart")
-    public void testToVerifyAddToCart3() {
-        testSteps.openingProduct();
-        testSteps.clickingAddToCart();
-        testSteps.checkingProductCount();
-    }
 
-    @Test(description = "Test to verify add to cart2")
-    public void testToVerifyAddToCart4()  {
-        testSteps.openingProduct();
-        testSteps.clickingAddToCart2();
-        testSteps.checkingProductCount();
-    }
 }
 
